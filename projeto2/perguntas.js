4f9965577b793bb0e8fb20b7181b4f91c2a68001
joaomarcos-09 <joao.bezerra25@escola.pr.gov.br>
2025-09-04 11:21:06 -0300

    .

---
+++ projeto2/perguntas.js
@@ -0,0 +1,20 @@
+criaCartao (
+'curiosidades',
+'Você sabe porque o frutiger-aero tem essa estetica?',
+'Ele tem essa estetica pois queriam simular um futuro "perfeito" com elementos da natureza',
+)
+
+
+criaCartao (
+conteudo,
+pergunta,
+resposta
+)
+
+   
+criaCartao (
+conteudo,
+pergunta,
+resposta
+)
+    
