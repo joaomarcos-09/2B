988d5c231a939a0d6d552f80f96578801b014979
joaomarcos-09 <joao.bezerra25@escola.pr.gov.br>
2025-09-11 11:08:13 -0300

    l

--- projeto2/perguntas.js
+++ projeto2/perguntas.js
@@ -6,8 +6,8 @@ criaCartao (
 
 
 criaCartao (
-'curiosidades'
-'Vocẽ sabe quem criou o frutiger-aero?'
+'curiosidades',
+'Vocẽ sabe quem criou o frutiger-aero?',
 ' termo Frutiger Aero foi criado em 2017 por Sofi Xian, do Consumer Aesthetics Research Institute, para descrever uma estética de design que combina a família de fontes Frutiger, do designer suíço Adrian Frutiger, com as características visuais do Windows Aero. '
 )
 
